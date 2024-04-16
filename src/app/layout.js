024f570c79595051ea3a50a563d432632fef1028
Harshal <harshalmukte@gmail.com>
2024-04-16 21:03:43 +0530

    add image

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,42 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+//for openGraph
+import ogImage from './opengraph-image.jpg'
 
 export const metadata = {
   title: "Harshal Mukte",
   description: "My Personal Portfolio App",
+  openGraph: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height
+      },
+    ],
+  },
   twitter: {
-    card: "summary_large_image"
-  }
-};
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height
+      },
+    ]
+  },
+}
+//for openGraph
+
+const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "Harshal Mukte",
+//   description: "My Personal Portfolio App",
+//   twitter: {
+//     card: "summary_large_image"
+//   }
+// };
 
 export default function RootLayout({ children }) {
   return (
